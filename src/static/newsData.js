7340c9337aa048a8c554a26492a9b84c830d78f2
devyako <yakhyoismoildjonov@gmail.com>
2020-04-03 10:36:11 +0500

    update

--- src/static/newsData.js
+++ src/static/newsData.js
@@ -2,12 +2,19 @@ export default [
   {
     id: 1,
     img:
+      "https://cdnuploads.aa.com.tr/uploads/Contents/2020/03/15/thumbs_b_c_a62adc4e0b55f65ed8f656478c9e90ae.jpg?v=190334",
+    title:
+      "(3.04.20): O`zbekistionda koronavirusga chalinganlar soni 16taga oshib 221 kishini tashkil etdi!"
+  },
+  {
+    id: 2,
+    img:
       "https://www.aljazeera.com/mritems/imagecache/mbdxxlarge/mritems/Images/2020/3/2/602a9359493248638608c4c7382eca0a_18.jpg",
     title:
       "(2.04.20): Xush habar, O‘zbekistonda koronavirusga chalinganlarning 13 nafari bu kassallikdan sog`ayib ketdi."
   },
   {
-    id: 2,
+    id: 3,
     img:
       "https://static.euronews.com/articles/stories/04/49/71/20/602x338_cmsv2_4837faef-a6b0-5183-bd55-10b23c909728-4497120.jpg",
     title:
